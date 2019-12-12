88a114ec6443d4f6b313251420631db98eb9b853
Shruthi K N <shruthikngowda@gmail.com>
2019-12-12 07:45:13 +0000

    Improvised variables names

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # two inputs, inputs checked for number,float format
 # operation restricted to + - * / and %. message if other than these inputs.
 
-uInput1 = input("Enter a number : ")
-uInput2 = input("Enter another number : ")
+user_input_number1 = input("Enter a number : ")
+user_input_number2 = input("Enter another number : ")
 uOperation = input("Enter the operation symbol : ")
 result = ""
 try:
     # TODO: write code...
-    number1 = float(uInput1)
-    number2 = float(uInput2)
+    number1 = float(user_input_number1)
+    number2 = float(user_input_number2)
 except ValueError:
     print("Please enter valid input numbers")
 else:
